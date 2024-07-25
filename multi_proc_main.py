ddc0c1780ee211b0a3decc7dc8035545b00f2825
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-07-25 11:52:14 +0100

    Played 2000 games

--- multi_proc_main.py
+++ multi_proc_main.py
@@ -1,11 +1,10 @@
-
 import gym
 import gym_simpletetris
 import numpy as np
 from ppo_torch import Agent
 from utils import plot_learning_curve
 
-env = gym.make('SimpleTetris-v0', reward_step=True)
+env = gym.make('SimpleTetris-v0', reward_step=True, high_scoring=True)
 N = 20
 batch_size = 64
 n_epochs = 4
@@ -13,10 +12,11 @@ alpha = 0.0003
 agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
                     alpha=alpha, n_epochs=n_epochs, 
                     input_dims=[env.observation_space.shape[0] * env.observation_space.shape[1]])
+filename = 'tetris2000games-noisynetworks'
 
 def run_worker(agent, process_num):
-    n_games = 500
-    figure_file = 'plots/tetris(factorisednoisynetwork-sigma=1-penaliseholes)500games.png'
+    n_games = 2000
+    figure_file = f'plots/{filename}.png'
     best_score = env.reward_range[0]
     score_history = []
     learn_iters = 0
@@ -48,6 +48,7 @@ def run_worker(agent, process_num):
 
         print('process_num: ', process_num, 'episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
                 'time_steps', n_steps, 'learning_steps', learn_iters)
+    np.savetxt(f'results/{filename}.txt', score_history, fmt='%d')
     x = [i+1 for i in range(len(score_history))]
     plot_learning_curve(x, score_history, figure_file)
 
