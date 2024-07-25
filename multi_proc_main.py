9d504871651cf00e3609ac885c3ddb685a480a27
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-07-25 13:12:36 +0100

    Tested 1000 games of noisy networks (sigma=1) against linear networks

--- multi_proc_main.py
+++ multi_proc_main.py
@@ -5,7 +5,7 @@ import numpy as np
 from ppo_torch import Agent
 from utils import plot_learning_curve
 
-env = gym.make('SimpleTetris-v0', reward_step=True, high_scoring=True)
+env = gym.make('SimpleTetris-v0', reward_step=True)
 N = 20
 batch_size = 64
 n_epochs = 4
@@ -13,10 +13,10 @@ alpha = 0.0003
 agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
                     alpha=alpha, n_epochs=n_epochs, 
                     input_dims=[env.observation_space.shape[0] * env.observation_space.shape[1]])
-filename = 'tetris2000games'
+filename = 'tetris-linearnetworks'
 
 def run_worker(agent, process_num):
-    n_games = 2000
+    n_games = 1000
     figure_file = f'plots/{filename}.png'
     best_score = env.reward_range[0]
     score_history = []
