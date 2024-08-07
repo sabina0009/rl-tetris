c9deedd9966111c93584bbdef0124060992ffcaf
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-08-07 14:40:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import gymnasium as gym
+import gym
 import gym_simpletetris
 import numpy as np
 from ppo_torch import Agent
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
                     alpha=alpha, n_epochs=n_epochs, 
                     input_dims=[env.observation_space.shape[0] * env.observation_space.shape[1]])
-    n_games = 1000
+    n_games = 10
     figure_file = 'plots/tetris.png'
     best_score = env.reward_range[0]
     score_history = []
@@ -30,8 +30,8 @@ if __name__ == '__main__':
         while not done:
             action, prob, val = agent.choose_action(observation)
             observation_, reward, terminated, truncated, info = env.step(action)
-            observation_ = observation_.flatten()
             done = terminated or truncated
+            observation_ = observation_.flatten()
             n_steps += 1
             score += reward
             agent.remember(observation, action, prob, val, reward, done)
@@ -46,8 +46,13 @@ if __name__ == '__main__':
             best_score = avg_score
             agent.save_models()
 
+        time_elapsed = time.time() - start_time
+        hours = time_elapsed // 3600
+        time_elapsed = time_elapsed % 3600
+        minutes = time_elapsed // 60
+        seconds = time_elapsed % 60
         print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
-                'time_steps', n_steps, 'learning_steps', learn_iters)
+                'time_steps', n_steps, 'learning_steps', learn_iters, 'runtime %d:%d:%.1f' % (hours, minutes, seconds))
     
     np.savetxt('results/tetris.txt', score_history, fmt='%d')
 
