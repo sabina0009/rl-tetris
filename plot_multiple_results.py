a5d168f7192447fad4896e46af65d7e65a59a91e
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-08-10 15:26:04 +0100

    Compared sb3 PPO to mine again

--- plot_multiple_results.py
+++ plot_multiple_results.py
@@ -1,3 +1,3 @@
 from utils import plot_multiple_learning_curves
 
-plot_multiple_learning_curves('plots/sb3-PPOvsDQN.png', 'results/sb3-tetris-PPO.txt', 'results/sb3-tetris-DQN.txt')
+plot_multiple_learning_curves('plots/sb3vsMine(Fixed).png', 'results/sb3-tetris-PPO.txt', 'results/tetris-100000timesteps.txt')
