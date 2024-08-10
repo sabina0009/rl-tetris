97d84b07ac29827edee9c31fccd1a62b5152afea
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-08-10 15:18:47 +0100

    Changed main to train on a certain amount of time steps instead of episodes

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
     agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
                     alpha=alpha, n_epochs=n_epochs, 
                     input_dims=[env.observation_space.shape[0] * env.observation_space.shape[1]])
-    n_games = 1000
+    #n_games = 1000
+    max_steps = 100000
     filename = 'tetris-separate-ac-networks'
     figure_file = f'plots/{filename}.png'
     best_score = env.reward_range[0]
@@ -22,8 +23,10 @@ if __name__ == '__main__':
     learn_iters = 0
     avg_score = 0
     n_steps = 0
+    episode = 0
     start_time = time.time()
-    for i in range(n_games):
+    #for i in range(n_games):
+    while n_steps < max_steps:
         observation, _ = env.reset()
         observation = observation.flatten()
         done = False
@@ -42,6 +45,7 @@ if __name__ == '__main__':
             observation = observation_
         score_history.append(score)
         avg_score = np.mean(score_history[-100:])
+        episode += 1
 
         if avg_score > best_score:
             best_score = avg_score
@@ -52,7 +56,7 @@ if __name__ == '__main__':
         time_elapsed = time_elapsed % 3600
         minutes = time_elapsed // 60
         seconds = time_elapsed % 60
-        print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
+        print('episode', episode, 'score %.1f' % score, 'avg score %.1f' % avg_score,
                 'time_steps', n_steps, 'learning_steps', learn_iters, 'runtime %d:%d:%.1f' % (hours, minutes, seconds))
     
     np.savetxt(f'results/{filename}.txt', score_history, fmt='%d')
