2789873c0a0df377a3eba930cadd2ccd0eb657a2
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-08-26 19:20:31 +0100

    Train 8 options

--- plot_curve.py
+++ plot_curve.py
@@ -3,6 +3,6 @@ from utils import plot_multiple_learning_curves, plot_average_learning_curve
 #plot_average_learning_curve('tetris-option-critic', 5)
 
 plot_multiple_learning_curves('plots/2-vs-4-options.png',
-                              'results/ppoc-2options/tetris-ppoc-average.txt',
-                              'results/pppoc-4options/tetris-ppoc-average.txt',
+                              'results/ppoc-8options/tetris-ppoc-8options-average.txt',
+                              'results/ppoc-4options/tetris-ppoc-average.txt',
 )
