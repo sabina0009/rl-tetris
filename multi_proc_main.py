aefe6933cbd32409a853d17a19d9d4ad02bf099d
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-08-27 18:02:57 +0100

    Ran 400,000 timesteps on 8x4 tetris board

--- multi_proc_main.py
+++ multi_proc_main.py
@@ -10,10 +10,10 @@ N = 2048
 batch_size = 32
 n_epochs = 10
 alpha = 0.0003
-filename = 'tetris-PPO'
+filename = 'tetris-PPO-8x4'
 
-def run_worker(process_num, score_history):
-    env = gym.make('SimpleTetris-v0', reward_step=True)
+def run_worker(process_num, score_history, lines_cleared_score):
+    env = gym.make('SimpleTetris-v0', height=8, width=4)
     agent = Agent(n_actions=env.action_space.n, filename=filename, batch_size=batch_size, 
                     alpha=alpha, n_epochs=n_epochs, 
                     input_dims=[env.observation_space.shape[0] * env.observation_space.shape[1]])
@@ -29,7 +29,7 @@ def run_worker(process_num, score_history):
     np.random.seed(process_num)
     #for i in range(n_games):
     while n_steps < max_steps:
-        observation, _ = env.reset()
+        observation, info = env.reset()
         observation = observation.flatten()
         done = False
         score = 0
@@ -46,6 +46,7 @@ def run_worker(process_num, score_history):
                 learn_iters += 1
             observation = observation_
         score_history[process_num].append(score)
+        lines_cleared_score[process_num].append(info['lines_cleared'])
         avg_score = np.mean(score_history[process_num][-100:])
         episode += 1
 
@@ -63,15 +64,19 @@ def run_worker(process_num, score_history):
                 'time_steps', n_steps, 'learning_steps', learn_iters, 'runtime %d:%d:%.1f' % (hours, minutes, seconds))
     
             np.savetxt(f'results/{filename}{process_num}.txt', score_history[process_num], fmt='%d')
+            np.savetxt(f'results/lines_scores/{filename}{process_num}.txt', lines_cleared_score[process_num], fmt='%d')
+
 
             x = [i+1 for i in range(len(score_history[process_num]))]
             plot_learning_curve(x, score_history[process_num], figure_file)
+            plot_learning_curve(x, lines_cleared_score[process_num],  f'plots/lines_scores/{filename}{process_num}.png')
 
 
     pass
 
 threads = 5
 score_history = [[] for i in range(threads)]
+lines_cleared_score = [[] for i in range(threads)]
 import torch.multiprocessing as mproc
 import threading
 if __name__ == '__main__':
@@ -80,7 +85,7 @@ if __name__ == '__main__':
     UPDATE_EVENT, ROLLING_EVENT = threading.Event(), threading.Event()
     ROLLING_EVENT.set()
     for process_num in range(threads):
-            p = mproc.Process(target=run_worker, args=(process_num, score_history))
+            p = mproc.Process(target=run_worker, args=(process_num, score_history, lines_cleared_score))
             p.start()
             processes.append(p)
     for p in processes:
