5ab930a66eb94c56f03fb5cdb2cd0058d9cb5103
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2024-09-01 17:21:42 +0100

    Adapted to merge into main

--- main_DQN.py
+++ main_DQN.py
@@ -6,26 +6,24 @@ from utils import plot_learning_curve, plot_average_learning_curve
 import time
 import os
 
-N = 20
-batch_size = 64
-n_epochs = 4
-alpha = 0.0003
+import torch.multiprocessing as mproc
+import threading
 
-env_name = 'tetris8x4'
-max_steps = 150000
 
-filename=f'{env_name}-dqn-{max_steps}steps'
-plot_path = f'plots/{filename}'
-results_path = f'results/{filename}'
-lines_path = f'results/{filename}/lines_cleared'
+def run_worker(process_num, score_history, lines_history, args, filename):
+    batch_size = 64
+    alpha = 0.0003
+    max_steps = args.steps
+    plot_path = f'plots/{filename}'
+    results_path = f'results/{filename}'
+    lines_path = f'results/{filename}'
 
-def run_worker(process_num, score_history, lines_cleared):
-    if env_name == 'tetris20x10':
+    if args.boardsize == '20x10':
         env = gym.make('SimpleTetris-v0', reward_step=True)
-    else:
+    elif args.boardsize == '8x4':
         env = gym.make('SimpleTetris-v0', height=8, width=4)
 
-    agent = Agent(n_actions=env.action_space.n, batch_size=batch_size,  
+    agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, alpha=alpha,
                     input_dims=[env.observation_space.shape[0] * env.observation_space.shape[1]])
     #n_games = 1000
     figure_file = f'plots/{filename}{process_num}.png'
@@ -61,9 +59,8 @@ def run_worker(process_num, score_history, lines_cleared):
         time_elapsed = time_elapsed % 3600
         minutes = time_elapsed // 60
         seconds = time_elapsed % 60
-        if episode % 50 == 0:
-            print('process_num', process_num, 'episode', episode, 'score %.1f' % score, 'avg score %.1f' % avg_score,
-                'time_steps', agent.num_steps, 'runtime %d:%d:%.1f' % (hours, minutes, seconds))
+        print('process_num', process_num, 'episode', episode, 'score %.1f' % score, 'avg score %.1f' % avg_score,
+            'time_steps', agent.num_steps, 'runtime %d:%d:%.1f' % (hours, minutes, seconds))
     
     try:
          os.mkdir(plot_path)
@@ -76,12 +73,12 @@ def run_worker(process_num, score_history, lines_cleared):
          pass
     
     try:
-         os.mkdir(lines_path)
+         os.mkdir(results_path)
     except:
          pass
     
     np.savetxt(f'{results_path}/{filename}-{process_num}.txt', score_history[process_num], fmt='%d')
-    np.savetxt(f'{lines_path}/{filename}-{process_num}.txt', lines_cleared[process_num], fmt='%d')
+    np.savetxt(f'{lines_path}/{filename}-{process_num}.txt', lines_history[process_num], fmt='%d')
 
     x = [i+1 for i in range(len(score_history[process_num]))]
     plot_learning_curve(x, score_history[process_num], f'{plot_path}/{filename}-{process_num}.png')
@@ -89,19 +86,16 @@ def run_worker(process_num, score_history, lines_cleared):
 
     pass
 
-threads = 5
-score_history = [[] for i in range(threads)]
-lines_cleared = [[] for i in range(threads)]
+def run_DQN(args, filename):
+    threads = 5
+    score_history = [[] for i in range(threads)]
+    lines_history = [[] for i in range(threads)]
 
-import torch.multiprocessing as mproc
-import threading
-if __name__ == '__main__':
-    #share the network weights between the processes
     processes = []
     UPDATE_EVENT, ROLLING_EVENT = threading.Event(), threading.Event()
     ROLLING_EVENT.set()
     for process_num in range(threads):
-            p = mproc.Process(target=run_worker, args=(process_num, score_history, lines_cleared))
+            p = mproc.Process(target=run_worker, args=(process_num, score_history, lines_history))
             p.start()
             processes.append(p)
     for p in processes:
